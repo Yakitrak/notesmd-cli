123e08b968d6e0366ac9c1f833c5197bae6a1696
Kuldip Satpute <satputekuldip@gmail.com>
2026-01-31 17:29:49 +0530

    Refactor ListEntries function and add new list.go
    - Simplified ListEntries by delegating to obsidian.ListEntries.
    - Removed unnecessary error handling and directory checks.
    - Added new list.go file to encapsulate list-related functionality.
    - Updated documentation images for usage and output.

--- pkg/actions/list.go
+++ pkg/actions/list.go
@@ -1,11 +1,6 @@
 package actions
 
 import (
-	"errors"
-	"os"
-	"sort"
-	"strings"
-
 	"github.com/Yakitrak/obsidian-cli/pkg/obsidian"
 )
 
@@ -24,45 +19,5 @@ func ListEntries(vault obsidian.VaultManager, params ListParams) ([]string, erro
 		return nil, err
 	}
 
-	targetPath := vaultPath
-	if strings.TrimSpace(params.Path) != "" {
-		validatedPath, err := obsidian.ValidatePath(vaultPath, params.Path)
-		if err != nil {
-			return nil, err
-		}
-		targetPath = validatedPath
-	}
-
-	info, err := os.Stat(targetPath)
-	if err != nil {
-		return nil, errors.New(obsidian.VaultAccessError)
-	}
-	if !info.IsDir() {
-		return nil, errors.New(obsidian.VaultAccessError)
-	}
-
-	entries, err := os.ReadDir(targetPath)
-	if err != nil {
-		return nil, errors.New(obsidian.VaultReadError)
-	}
-
-	dirs := make([]string, 0, len(entries))
-	files := make([]string, 0, len(entries))
-
-	for _, entry := range entries {
-		name := entry.Name()
-		if strings.HasPrefix(name, ".") {
-			continue
-		}
-		if entry.IsDir() {
-			dirs = append(dirs, name+"/")
-			continue
-		}
-		files = append(files, name)
-	}
-
-	sort.Strings(dirs)
-	sort.Strings(files)
-
-	return append(dirs, files...), nil
+	return obsidian.ListEntries(vaultPath, params.Path)
 }
