723cfa8481de5a7284a9fab23cc9961bf9844629
Michael Sale <2355139+mikesale@users.noreply.github.com>
2026-02-08 11:15:31 -0700

    added scanning of paths for flatpak & snap install (#76)

--- pkg/config/obsidian_path.go
+++ pkg/config/obsidian_path.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"errors"
+	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func ObsidianFile() (obsidianConfigFile string, err error) {
@@ -10,6 +12,37 @@ func ObsidianFile() (obsidianConfigFile string, err error) {
 	if err != nil {
 		return "", errors.New(UserConfigDirectoryNotFoundErrorMessage)
 	}
-	obsidianConfigFile = filepath.Join(userConfigDir, ObsidianConfigDirectory, ObsidianConfigFile)
-	return obsidianConfigFile, nil
+
+	defaultPath := filepath.Join(userConfigDir, ObsidianConfigDirectory, ObsidianConfigFile)
+	var candidatePaths []string
+
+	switch runtime.GOOS {
+	case "linux":
+		candidatePaths = append(candidatePaths, defaultPath)
+
+		homeDir, homeErr := os.UserHomeDir()
+		if homeErr == nil {
+			candidatePaths = append(candidatePaths,
+				filepath.Join(homeDir, ".var", "app", "md.obsidian.Obsidian", "config", "obsidian", ObsidianConfigFile))
+			candidatePaths = append(candidatePaths,
+				filepath.Join(homeDir, "snap", "obsidian", "current", ".config", "obsidian", ObsidianConfigFile))
+		}
+	default:
+		return defaultPath, nil
+	}
+
+	var firstNonExistErr error
+	for _, path := range candidatePaths {
+		if _, statErr := os.Stat(path); statErr == nil {
+			return path, nil
+		} else if !os.IsNotExist(statErr) && firstNonExistErr == nil {
+			firstNonExistErr = statErr
+		}
+	}
+
+	if firstNonExistErr != nil {
+		return "", firstNonExistErr
+	}
+
+	return defaultPath, nil
 }
